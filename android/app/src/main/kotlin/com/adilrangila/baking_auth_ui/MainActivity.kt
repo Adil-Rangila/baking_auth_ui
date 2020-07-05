75a0be85910bb3700fcf996cc47a39cb2441414e
Adil Ahmed <adil.rangila@gmail.com>
2020-07-05 16:01:26 +0500

    Sign In page is completed and app is finished.

---
+++ android/app/src/main/kotlin/com/adilrangila/baking_auth_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adilrangila.baking_auth_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:baking_auth_ui/constants.dart';
+import 'package:baking_auth_ui/sign_in_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -24,6 +25,13 @@ class MyApp extends StatelessWidget {
           headline5:
               TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
           button: TextStyle(color: kPrimaryColor),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white.withOpacity(0.3),
+            ),
+          ),
         ),
       ),
       home: WelcomeScreen(),
@@ -68,32 +76,42 @@ class WelcomeScreen extends StatelessWidget {
                   ),
                 ),
                 FittedBox(
-                  child: Container(
-                    margin: EdgeInsets.only(bottom: 15.0),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25.0),
-                      color: kPrimaryColor,
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          'Starts Learning',
-                          style: Theme.of(context).textTheme.button.copyWith(
-                                color: Colors.black,
-                              ),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignInScreen(),
                         ),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Icon(
-                          Icons.arrow_forward,
-                          color: Colors.black,
-                        ),
-                      ],
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 15.0),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: kPrimaryColor,
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Starts Learning',
+                            style: Theme.of(context).textTheme.button.copyWith(
+                                  color: Colors.black,
+                                ),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
